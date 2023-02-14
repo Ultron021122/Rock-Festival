dce391279a43391a6ae8b153b98acc0c1c34634a
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2023-02-14 11:33:06 -0600

    version 2.2

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 // Imagenes 
+const cache = require('gulp-cache');
+const imagenin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -15,6 +17,17 @@ function css(done) {
         .pipe(dest("build/css")); // Almacenarlo en el disco duro
  
     done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagenin(opciones) ) )
+        .pipe( dest('build/img'));
+    done();
 }
 
 function versionWebp( done ) {
@@ -35,5 +48,6 @@ function dev( done ) {
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel( versionWebp, dev);
+exports.dev = parallel( imagenes, versionWebp, dev);
